4a198a9ada66facad88799004a7e238acba124ef
maramalquraan <quraanmaram@gmail.com>
2017-08-26 09:26:32 +0300

    2nd

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,9 +1,19 @@
 // your code here
-.first(array){
-return array[0];
+
+function array(arr){
+	var array = arr;
+
+	return{
+		first: function ()
+		{
+			return array[0];
+		},
+
+		last: function ()
+		{
+			return array[array.length-1];
+		}
+	}
+
 }
 
-.last(array){
-
-	return array[array.length-1];
-}
